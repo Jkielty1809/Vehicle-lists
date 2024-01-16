39a8840867887cc3518713a122f156c166db26e6
User <User@DESKTOP-S6VC1E7>
2024-01-16 09:22:55 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Vehicle_lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
